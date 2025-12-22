85169cc0c2268dc8de33e44ef1dd3ad53eb019b6
Shadda-raf <borislav3001@gmail.com>
2025-12-22 12:00:20 +0300

    test task

---
+++ Pages/en.eventcalendar.spec.ts
@@ -0,0 +1,66 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+
+export class EventCalendarEnPage{
+
+    readonly page: Page;
+    readonly topicSelector: Locator;
+    readonly countrySelector: Locator;
+    readonly widthField: Locator;
+    readonly widthRangeInput: Locator;
+    readonly widthContainerCheckbox: Locator;
+    readonly heightField: Locator;
+    readonly heightRangeInput: Locator;
+    readonly heightContainerCheckbox: Locator;
+    readonly blueThemeRb: Locator;
+    readonly greenThemeRb: Locator;
+    readonly turquoiseThemeRb: Locator;
+    readonly purpleThemeRb: Locator;
+    readonly codeField: Locator;
+    readonly copyCodeBtn: Locator;
+    readonly generatePreviewBtn: Locator;
+    readonly previewField:Locator;
+    readonly promocodesTrak: Locator;
+    readonly languageSwitcher: Locator;
+    readonly blueThemeRbParent: Locator;
+    readonly greenThemeRbParent: Locator;
+    readonly turquoiseThemeRbParent: Locator;
+    readonly purpleThemeRbParent: Locator;
+    readonly iframeLocator:Locator;
+
+    constructor(page: Page) {
+    this.page = page;
+    this.languageSwitcher = page.locator('dropdown', {hasText: 'EN'})
+    this.topicSelector = page.locator('.checkselect', {hasText: 'Select a topic'});
+    this.countrySelector = page.locator('.checkselect', {hasText: 'All countries'});
+    this.widthField = page.locator('input[name="width"]');
+    this.widthRangeInput = page.locator('#width-range');
+    this.widthContainerCheckbox = page.locator('a', {hasText: 'the entire width of the container'});
+    this.heightField = page.locator('input[name="height"]');
+    this.heightRangeInput = page.locator('#height-ran[]ge');
+    this.heightContainerCheckbox = page.locator('a', {hasText: 'to the full height of the block'});
+   this.blueThemeRb = page.locator('input[type="radio"][value="blue"]');
+    this.blueThemeRbParent = page.locator('.radio').filter({ has: this.blueThemeRb });
+    this.greenThemeRb= page.locator('input[type="radio"][value="green"]');
+    this.greenThemeRbParent = page.locator('.radio').filter({ has: this.greenThemeRb });
+    this.turquoiseThemeRb= page.locator('input[type="radio"][value="turquoise"]');
+    this.turquoiseThemeRbParent = page.locator('.radio').filter({ has: this.turquoiseThemeRb });
+    this.purpleThemeRb= page.locator('input[type="radio"][value="purple"]');
+    this.purpleThemeRbParent = page.locator('.radio').filter({ has: this.purpleThemeRb });
+    this.codeField = page.locator('#code');
+    this.copyCodeBtn = page.locator('#code-copy-button');
+    this.generatePreviewBtn = page.locator('button', {hasText: 'Generate preview'});
+    this.previewField = page.locator('#preview');
+    this.promocodesTrak = page.locator('.promocodes-carousel');
+    this.iframeLocator = page.locator('iframe[id="3snet-frame"]');
+    }
+
+
+    async goto() {
+        await this.page.goto('https://dev.3snet.info/en/eventswidget/');
+    }
+    
+    
+
+}
+
+
--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -36,7 +36,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] }
     },
 
     {
